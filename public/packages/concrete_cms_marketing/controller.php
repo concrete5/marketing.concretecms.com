c773dbaa41e4780401871973fbee7772105eb0ca
Fabian Bitter <fabian@bitter.de>
2021-05-03 17:31:16 +0200

    Rollback most of the changes because they are moved to the themes reposistory (required for the default page type)

--- public/packages/concrete_cms_marketing/controller.php
+++ public/packages/concrete_cms_marketing/controller.php
@@ -3,9 +3,6 @@
 namespace Concrete\Package\ConcreteCmsMarketing;
 
 use Concrete\Core\Package\Package;
-use Concrete\Core\Page\PageList;
-use Concrete\Core\Page\Summary\Template\Populator;
-use PortlandLabs\ConcreteCmsMarketing\Provider\ServiceProvider;
 
 class Controller extends Package
 {
@@ -28,39 +25,16 @@ class Controller extends Package
         return t("marketing.concretecms.com");
     }
 
-    public function on_start()
-    {
-        /** @var ServiceProvider $serviceProvider */
-        $serviceProvider = $this->app->make(ServiceProvider::class);
-        $serviceProvider->register();
-    }
-
-    private function postInstall()
-    {
-        /*
-         * This is required to update the available page templates
-         */
-        /** @var Populator $populator */
-        $populator = $this->app->make(Populator::class);
-        /** @var PageList $pageList */
-        $pageList = $this->app->make(PageList::class);
-        foreach ($pageList->getResults() as $page) {
-            $populator->updateAvailableSummaryTemplates($page);
-        }
-    }
-
     public function install()
     {
         parent::install();
         $this->installContentFile('data.xml');
         $this->installContentFile('content.xml');
-        $this->postInstall();
     }
 
     public function upgrade()
     {
         parent::upgrade();
         $this->installContentFile('data.xml');
-        $this->postInstall();
     }
 }
